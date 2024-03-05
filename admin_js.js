a7dde83021a7914802abf5e6fb12f568b2319235
“ayush <ayushsaklani04@gmail.com>
2024-03-06 00:04:58 +0530

    06-03-2024

--- admin_js.js
+++ admin_js.js
@@ -2,15 +2,24 @@
 // import {Html5QrcodeScanner} from "html5-qrcode";
 var data=[];
 var audio = new Audio('/media/smb_coin.wav');
+function check_inArray(xx){
+    var count=data.length;
+    for(var i=0;i<count;i++){
+        if(data[i][3]===xx){return true;}
+    }
+    return false;
+}
 function onScanSuccess(decodedText) {
     let temp = (decodedText).split(','); 
-    let temparr = []
-    for(i in temp){
-        temparr.push(temp[i]);
+    if(!check_inArray(temp[3])){
+        let temparr = []
+        for(i in temp){
+            temparr.push(temp[i]);
+        }
+        data.push(temparr);
+        audio.play();
+        console.log(data[0]);
     }
-    data.push(temparr);
-    audio.play();
-    console.log(data[0]);
 }
 let html5QrcodeScanner = new Html5QrcodeScanner(
   "reader",
